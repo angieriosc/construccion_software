dc300fcd9d30b24b1601b6f50cdbcc9a29261120
Angie <A01705651@tec.mx>
2024-03-18 23:02:38 -0600

    angie_lab19

--- lab_15_16_17_18_19/app.js
+++ lab_15_16_17_18_19/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const session = require("express-session");
 
-app.use(
-  session({
+app.use(session({
     secret:
       "top08dilu28934jjbr4u3ruifsdkhf4urhehhuhrieuwhrsndfb",
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
   })
 );
-
-app.set("view engine", "ejs");
-app.set("views", "views");
 
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,21 +21,20 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Agregar protección contra ataques de CSRF
 const csrf = require('csurf');
 const csrfProtection = csrf();
-
 //...Y después del código para inicializar la sesión... 
 app.use(csrfProtection); 
-
-const rutasUsuarios = require("./routes/usuarios.routes");
-app.use("/users", rutasUsuarios);
-
 
 //Middleware
 app.use((request, response, next) => {
   console.log("Middleware!");
   next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
+
+const rutasUsuarios = require("./routes/usuarios.routes");
+app.use("/users", rutasUsuarios);
 
 //Registrar el middleware con el módulo construcciones
 const rutasConstrucciones = require("./routes/construcciones.routes");
